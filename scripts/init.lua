ce20f8bc409453e83cd32ad29d7f7b78a333feb4
Brett <brett@bmandesigns.com>
2019-10-08 01:20:52 -0600

    main addon

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,3 +1,3 @@
 fs_copy("./.hemtt/template/include", "./include")
-fs_copy("./.hemtt/template/addons/main", "./addons/")
+fs_copy("./.hemtt/template/addons/main", "./addons/main")
 fs_copy("./.hemtt/template/mod.ht.cpp", "./mod.ht.cpp")
